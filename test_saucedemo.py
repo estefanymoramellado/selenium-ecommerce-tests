95538b457a2d027bd3a2082426811d73b5c03668
estefany mora <noir.desir.sublime@gmail.com>
2026-06-19 11:30:33 -0400

    Agrega tests de login fallido y campos vacios (casos negativos)

--- test_saucedemo.py
+++ test_saucedemo.py
@@ -26,6 +26,33 @@ def test_login_exitoso(driver):
     assert "inventory" in driver.current_url
 
 
+def hacer_login_incorrecto(driver):
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located((By.ID, "user-name"))).send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("password_fail")
+    driver.find_element(By.ID, "login-button").click()
+    wait.until (EC.presence_of_element_located((By.CSS_SELECTOR, "[data-test='error']")))
+
+def test_login_error(driver):
+    hacer_login_incorrecto(driver)
+    error = driver.find_element(By.CSS_SELECTOR, "[data-test='error']")
+    assert error.is_displayed()
+    assert "inventory" not in driver.current_url
+
+def hacer_login_vacio(driver):
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located((By.ID, "user-name"))).send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("")
+    driver.find_element(By.ID, "login-button").click()
+    wait.until (EC.presence_of_element_located((By.CSS_SELECTOR, "[data-test='error']")))
+
+def test_login_vacio(driver):
+    hacer_login_vacio(driver)
+    error = driver.find_element(By.CSS_SELECTOR, "[data-test='error']")
+    assert error.is_displayed()
+    assert "inventory" not in driver.current_url
+
+
 def test_inventario_muestra_productos(driver):
     hacer_login(driver)
     wait = WebDriverWait(driver, 10)
